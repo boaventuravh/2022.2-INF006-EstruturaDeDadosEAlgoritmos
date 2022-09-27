fada241b995fbe2d23dce6409c6d49208f85fb0b
boaventuravh <111531346+boaventuravh@users.noreply.github.com>
2022-09-27 21:12:53 +0000

    Adicionadas questões 3 e 4

--- ListaRevisao02INF006-VitorHugo/q03.c
+++ ListaRevisao02INF006-VitorHugo/q03.c
@@ -2,7 +2,8 @@
 repetidos. Gere um vetor VET1 que tambem será ordenado e terá somente os elementos do vetor VET que não sao repetidos.*/
 
 #include <stdio.h>
-#define QTDE 3 // !!!!!!MUDAR PRA 10 DEPOIS!!!!!!!!!!!
+#define QTDE 20
+
 
 int main()
 {
@@ -16,12 +17,46 @@ int main()
         printf("\nDigite o %do valor: ", i+1);
         scanf("%d", &vet1[i]);
     }
-    
-    for(int i=0, i<QTDE; i++)
-        if(indiceRepeat[i] == 0)
-            for(int j = i+1; j<QTDE; j++)
+
+   
+    for(int i=0; i<QTDE; i++)
+	{
+		for(int j = i+1; j<QTDE; j++)
+            if(indiceRepeat[i] == 0)
             {
-                
+                if (vet1[i] == vet1[j])
+				{
+					indiceRepeat[i]++;
+					if(indiceRepeat[j] == 0)
+						indiceRepeat[j]++;
+				}
             }
+	}
+	
+	int qtdeVet2 = 0; // contagem da quantidade de elementos de vet2
+	
+	// i percorre o vetor indiceRepeat, j percorre vet1
+	for(int i=0, j=0; i<QTDE; i++) 
+		if(indiceRepeat[i] == 0)
+		{
+			vet2[j]=vet1[i];
+			j++;
+			qtdeVet2++;
+		}
+	
+	for(int i = 1;i<qtdeVet2;i++)
+		for(int j = 0, aux=0;j<qtdeVet2-1;j++)
+			if(vet2[j]>vet2[j+1])
+			{
+				aux = vet2[j];
+				vet2[j] = vet2[j+1];
+				vet2[j+1] = aux;
+			}
+
+	printf("\nEis o vetor 2 ordenado: ");
+
+	for(int i = 0;i<qtdeVet2;i++)
+		printf("%8d", vet2[i]);
+	
     return 0;
 }
---
+++ ListaRevisao02INF006-VitorHugo/q04.c
@@ -0,0 +1,41 @@
+/*4) Que armazene 10 numeros em um vetor. Na entrada de dados, o numero ja deve ser armazenado na sua posição definitiva em ordem decrescente. imprimir o vetor logo apos a entrada de dados;*/
+
+#include <stdio.h>
+#define QTDE 5 
+// !!!!!!!MUDAR QTDE PRA 10 DEPOIS!!!!!!
+
+int main(){
+
+	int vet[QTDE];
+
+	for (int i = 0; i < QTDE; ++i)
+		vet[i]=0;
+
+	for(int count=0;count<QTDE;count++)
+	{
+		printf("\nDigite o %do valor: ", count+1);
+		scanf("%d", &vet[count]);
+		for (int i = 0; i < QTDE; ++i)
+		 {
+			
+		   	for (int j = i + 1; j < QTDE; ++j)
+		   	{
+				if (vet[i] < vet[j])
+				    {
+				     int a = vet[i];
+				     vet[i] = vet[j];
+				     vet[j] = a;
+				   	}
+		  	}
+		 }
+		printf("\nOrdem decrescente:\n");
+			if(vet[count] != 0)
+				for(int k=0;k<count+1;k++)
+					printf("%8d", vet[k]);
+	}
+
+ 	
+	
+	
+	return 0;
+}
